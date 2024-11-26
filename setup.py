722a1ae5e53181bdbb7aeb770d4c8d9c30fab922
张烨槟 <2947436155@qq.com>
2024-11-26 16:06:29 +0800

    Add Ascend installation adapter

--- setup.py
+++ setup.py
@@ -4,6 +4,15 @@ import sys
 
 from setuptools import find_packages, setup
 
+npu_available = False
+try:
+    import torch
+    import torch_npu
+
+    npu_available = torch.npu.is_available()
+except:
+    pass
+
 pwd = os.path.dirname(__file__)
 version_file = 'lmdeploy/version.py'
 
@@ -145,9 +154,9 @@ if __name__ == '__main__':
         include_package_data=True,
         setup_requires=parse_requirements('requirements/build.txt'),
         tests_require=parse_requirements('requirements/test.txt'),
-        install_requires=parse_requirements('requirements/runtime.txt'),
+        install_requires=parse_requirements('requirements/runtime_ascend.txt' if npu_available else 'requirements/runtime.txt'),
         extras_require={
-            'all': parse_requirements('requirements.txt'),
+            'all': parse_requirements('requirements_ascend.txt' if npu_available else 'requirements.txt'),
             'lite': parse_requirements('requirements/lite.txt'),
             'serve': parse_requirements('requirements/serve.txt')
         },
